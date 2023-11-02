fb93433f52269a7ad559ec41c3533b63e2fcdd83
Nguyen Thi Ngoc Bich <93378230+NgocBich204@users.noreply.github.com>
2023-11-02 23:02:03 +0700

    Update: page home.

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $(".image-slider").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'>></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'>></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
+
+ 
